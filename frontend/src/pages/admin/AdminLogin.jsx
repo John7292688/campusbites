45446fbdf179e8a8be99f5e423b1546cc0139adc
John7292688 <johnfaith493@gmail.com>
2026-09-10 16:25:39 +0100

    lgin

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -19,7 +19,7 @@ function AdminLogin() {
 
     try {
       const response = await axios.post(
-        "${import.meta.env.VITE_API_URL}/api/admin/login",
+        `${import.meta.env.VITE_API_URL}/api/admin/login`,
         {
           email,
           password,
